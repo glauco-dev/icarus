d99afbde21476be6da39ff201675b972f43b551f
glauco-dev <icaroglauco@email.com>
2023-12-08 03:38:00 -0300

    Implementado a api que retornará a aplicação node no futuro
    Please enter the commit message for your changes. Lines starting
    with '#' will be ignored, and an empty message aborts the commit.
    
    On branch master
    Your branch is up to date with 'origin/master'.
    
    Changes to be committed:
    	modified:   package.json
    	new file:   src/api/index.js
    	new file:   src/api/pocketbase.exe

---
+++ src/api/index.js
@@ -0,0 +1,12 @@
+const { exec } = require('child_process');
+
+module.exports = (req, res) => {
+  exec('/pocketbase.exe', (error, stdout, stderr) => {
+    if (error) {
+      console.error(`Erro ao executar o binário: ${error}`);
+      return res.status(500).send('Erro interno do servidor');
+    }
+    console.log(`Saída do binário: ${stdout}`);
+    res.status(200).send('Binário executado com sucesso');
+  });
+};
