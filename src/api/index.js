63ebff825fafc01368cafbf2b77c263f65a97cc2
glauco-dev <icaroglauco@email.com>
2023-12-08 04:03:46 -0300

    Estado de pronto da api que executa Testando node com o bundle mjs
    Please enter the commit message for your changes. Lines starting
    with '#' will be ignored, and an empty message aborts the commit.
    
    On branch master
    Your branch is up to date with 'origin/master'.
    
    Changes to be committed:
    	modified:   src/api/index.js
    	new file:   vercel.json

--- src/api/index.js
+++ src/api/index.js
@@ -2,7 +2,7 @@ const { exec } = require('child_process');
 const app = require('express')();
 const { v4 } = require('uuid');
 
-app.get('/', (req, res) => {
+app.get('/api', (req, res) => {
   // const path = `/api/item/${v4()}`;
   // res.setHeader('Content-Type', 'text/html');
   // res.setHeader('Cache-Control', 's-max-age=1, stale-while-revalidate');
