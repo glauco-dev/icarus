e8ddf6ab7152e044ea8a82054117b15c5dabf8f9
glauco-dev <icaroglauco@email.com>
2023-12-08 02:37:52 -0300

    Please enter the commit message for your changes. Lines starting with '#' will be ignored, and an empty message aborts the commit.
    On branch master
    Your branch is up to date with 'origin/master'.
    
    Changes to be committed:
    	modified:   package-lock.json
    	modified:   package.json
    	new file:   src/Fontfabric - GloberLight.otf
    	new file:   src/Fontfabric - GloberRegular.otf
    	new file:   src/config.js
    	new file:   src/config.scss

---
+++ src/config.js
@@ -0,0 +1,13 @@
+export const links = [
+    {path: '/', label: 'Início'},
+    {path: '/afiliado', label: 'Afiliação'},
+    {path: '/agenda', label: 'Agenda'},
+    {label: 'Sobre', 
+        links:[
+            {path: '/institucional#historia', label: 'História'},
+            {path: '/institucional#estatudo', label: 'Estatudo'},
+            {path: '/institucional#diretoria', label: 'Diretoria'},
+            {path: '/institucional#legislativo', label: 'Legislação'},
+            {path: '/institucional#creches-e-escolas', label: 'Creches e escolas'},
+        ]},
+]
