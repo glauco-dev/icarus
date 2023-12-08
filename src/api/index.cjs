6bede55ab80da238beab25e3cf59f7bd9605549f
glauco-dev <icaroglauco@email.com>
2023-12-08 06:32:31 -0300

    Compatibilidade do projeto com mjs para se hospedar por meio de ssr-to-html na resposta do node index.mjs Changes to be committed: 	modified:   .gitignore 	new file:   npm 	modified:   package.json 	modified:   src/api/index.cjs

--- src/api/index.cjs
+++ src/api/index.cjs
@@ -11,14 +11,14 @@ app.get('/api', (req, res) => {
   // res.setHeader('Cache-Control', 's-max-age=1, stale-while-revalidate');
   // res.end(`Hello! Go to item: <a href="${path}">${path}</a>`);
   return new PocketBase();
-  exec('/pocketbase serve', (error, stdout, stderr) => {
-    if (error) {
-      console.error(`Erro ao executar o binário: ${error}`);
-      return res.status(500).send('Erro interno do servidor');
-    }
-    console.log(`Saída do binário: ${stdout}`);
-    res.status(200).send('Binário executado com sucesso');
-  });
+  // exec('/pocketbase serve', (error, stdout, stderr) => {
+  //   if (error) {
+  //     console.error(`Erro ao executar o binário: ${error}`);
+  //     return res.status(500).send('Erro interno do servidor');
+  //   }
+  //   console.log(`Saída do binário: ${stdout}`);
+  //   res.status(200).send('Binário executado com sucesso');
+  // });
 });
 
 
